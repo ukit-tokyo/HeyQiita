c70b3f9303ab909b13e0f62bca159f135327a431
ukit-tokyo <t.yuki@ukit.tokyo>
2021-12-23 00:09:58 +0900

    [add] create project

---
+++ HeyQiita/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeyQiita
+//
+//  Created by 行木 太一 on 2021/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeyQiita/HeyQiitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyQiitaApp.swift
+//  HeyQiita
+//
+//  Created by 行木 太一 on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HeyQiitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
