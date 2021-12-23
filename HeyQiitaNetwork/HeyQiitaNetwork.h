7725efad1b684613b1bd54b8c76236c63a76df9d
ukit-tokyo <t.yuki@ukit.tokyo>
2021-12-23 23:55:46 +0900

    [add] Embedded Frameworks "Core" "Network"

---
+++ HeyQiitaNetwork/HeyQiitaNetwork.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeyQiitaNetwork.
+FOUNDATION_EXPORT double HeyQiitaNetworkVersionNumber;
+
+//! Project version string for HeyQiitaNetwork.
+FOUNDATION_EXPORT const unsigned char HeyQiitaNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeyQiitaNetwork/PublicHeader.h>
+
+
