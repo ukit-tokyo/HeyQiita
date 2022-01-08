a298baff33ce263ca8692c348b489278b400e945
ukit-tokyo <t.yuki@ukit.tokyo>
2022-01-09 03:19:50 +0900

    [add] ログイン画面のレイアウトを実装

--- HeyQiita/HeyQiitaApp.swift
+++ HeyQiita/HeyQiitaApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct HeyQiitaApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      LoginView()
     }
   }
 }
--- HeyQiita/View/Pages/LoginView.swift
+++ HeyQiita/View/Pages/LoginView.swift
@@ -1,20 +1,20 @@
-//
-//  LoginView.swift
-//  HeyQiita
-//
-//  Created by 行木 太一 on 2022/01/09.
-//
-
 import SwiftUI
 
+/// ログイン画面
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    VStack(spacing: 150) {
+      AppLogo(fontSize: 50)
+      HStack(spacing: 20) {
+        AppButton(title: "Login", action: {})
+        TextButton(title: "Skipping", action: {})
+      }
     }
+  }
 }
 
 struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
+  static var previews: some View {
+    LoginView()
+  }
 }
